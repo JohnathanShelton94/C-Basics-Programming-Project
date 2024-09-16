197353b5a30bbffef6d9bf8dfd93c00223b32996
Johnathan Shelton <js24634@email.vccs.edu>
2024-09-16 17:29:04 -0400

    Add project files.

---
+++ C++ Basics Programming Project.cpp
@@ -0,0 +1,9 @@
+// C++ Basics Programming Project.cpp : This file contains the code for the C++ Basics Programming Project
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "C++ Basics Programming Project Code!!\n";
+}
