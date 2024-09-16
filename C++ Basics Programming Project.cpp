894981daa6b84488018333f030f40785a6187287
Johnathan Shelton <js24634@email.vccs.edu>
2024-09-16 17:36:06 -0400

    Adding comments to text

--- C++ Basics Programming Project.cpp
+++ C++ Basics Programming Project.cpp
@@ -1,19 +1,19 @@
-
+//This program will calculate and display the distance the car can travel on one tank of gas when driven in town and when driven on the highway.
 
 #include <iostream>
 using namespace std;
 
 int main() {
-    // Declare variables
-    double tankSize = 20.0; // 20-gallon gas tank
-    double mpgTown = 23.5;  // Miles per gallon in town
-    double mpgHighway = 28.9; // Miles per gallon on highway
+        // Declare variables
+    double tankSize = 20.0;     // 20-gallon gas tank
+    double mpgTown = 23.5;      // Miles per gallon in town
+    double mpgHighway = 28.9;     // Miles per gallon on highway
 
-    // Calculate the distance in town and on highway
+        // Calculate the distance in town and on highway
     double distanceTown = tankSize * mpgTown;
     double distanceHighway = tankSize * mpgHighway;
 
-    // Display the results
+        // Display the results
     cout << "The distance the car can travel on one tank of gas:" << endl;
     cout << "In town: " << distanceTown << " miles" << endl;
     cout << "On the highway: " << distanceHighway << " miles" << endl;
